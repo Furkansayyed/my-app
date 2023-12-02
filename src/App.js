7865959ac4b9b433df34ea1353a1d3abda7ec38e
Furkan Sayyed <sayyedfurkan115@gmail.com>
2023-12-02 22:03:33 +0530

    All changes done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import { HashRouter , Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
